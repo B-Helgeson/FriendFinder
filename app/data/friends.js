715b354dc548dbf3e29e456172e5edf0bd2d155d
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-05-12 17:36:54 -0400

    updated js in the api routes and survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,18 @@
-var friendsData = [
+  var friendsData = [
     {
-      friendName: "Aimes",
-      friendScore: "7",
+      friendName: 'Terry',
+      friendPhoto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/1200px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+      friendScores: ['2','1','2','5','2','1','4','1','3','2']
+    },
+    {
+      friendName: 'Amy Santiago',
+      friendPhoto: 'https://vignette.wikia.nocookie.net/brooklynnine-nine/images/4/44/Melissa-509.jpg/revision/latest/scale-to-width-down/168?cb=20130516235302',
+      friendScores: ['1','3','3','2','4','5','3','4','5','1']
+    },
+    {
+      friendName: 'Jake Peralta',
+      friendPhoto: 'https://pbs.twimg.com/profile_images/928266336073117696/W2gCidjA_400x400.jpg',
+      friendScores: ['5','5','1','3','4','5','3','4','2','5']
     }
   ];
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,35 @@ const   friendsData = require("../data/friends");
 
 module.exports = function(app) {
 
+  // API GET to return all friends data
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
+  // Accepts API POST requests with new user data
   app.post("/api/friends", function(req, res) {
-// Code to post a new friend to the data array...
+
+    let userInput = req.body,
+        totalDifference = 0,
+        allDifferences = 0;
+
+    for (var i = 0; i<(friendsData.length-1); i++){
+      for (var q = 0; q<10; q++){
+        totalDifference += Math.abs(friendsData[i].friendscores[q] - userInput[q]);
+      }
+    
+
+      allDifferences.push(totalDifference);
+
+      totalDifference = 0;
+
+    }
+
+    let match = friendsData[allDifferences.indexOf(Math.min.apply(null, allDifferences))];
+
+    res.send(match);
+    console.log(match);
+
   });
 
 
